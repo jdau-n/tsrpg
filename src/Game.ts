879a08071f0162e9c013a2bf30265db81e3eec2b
James Demmery <james.demmery@thedistillery.co>
2023-01-21 13:05:29 +1100

    remove some experimental code

--- src/Game.ts
+++ src/Game.ts
@@ -1,17 +1,9 @@
-import TabManager from "./ui/TabManager"
-import StatusManager from "./ui/StatusManager"
-
 export default class Game {
 
     intervalHandle: number;
 
-    statusManager: StatusManager;
-
     init() {
         this.intervalHandle = window.setInterval( this.tick, 1000 );
-
-        this.statusManager = new StatusManager();
-        this.statusManager.createDOM();
     }
 
     tick() {
