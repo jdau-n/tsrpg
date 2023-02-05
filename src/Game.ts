e02745acdf5792840cdd2e42790d051c0cd26685
James Demmery <james.demmery@thedistillery.co>
2023-02-06 01:58:20 +1100

    zzz

--- src/Game.ts
+++ src/Game.ts
@@ -18,19 +18,77 @@ export default class Game {
     }
 }
 
+class Enemy {
+    health: number;
+    currentHealth: number;
+    damage: number;
+    defence: number;
+    dodge: number;
+    block: number;
+    speed: number;
+
+    genStats( powerLevel:number ) {
+        
+    }
+}
+
 class UIManager {
 
-    buttonTabFight: HTMLButtonElement;
+    buttonTabTown: HTMLButtonElement;
+    buttonTabAdventure: HTMLButtonElement;
+    buttonTabInventory: HTMLButtonElement;
+    
+    buttonTabOptions: HTMLButtonElement;
+    tabTown: HTMLElement;
+    tabAdventure: HTMLElement;
+    tabInventory: HTMLElement;
+    tabOptions: HTMLElement;
 
     init() {
-        this.buttonTabFight = document.querySelector("#fight-button");
-        
-        //this.buttonTabFight.addEventListener("click", function() { this.switchTab("fight") }.bind(this) );
-        this.buttonTabFight.addEventListener("click", () => { this.switchTab("fight") } );
-    }
+        this.buttonTabTown = document.querySelector("#town-button");
+        this.buttonTabTown.addEventListener("click", () => this.switchTab("town") );
 
-    switchTab(something: string) {
+        this.buttonTabAdventure = document.querySelector("#adventure-button");
+        this.buttonTabAdventure.addEventListener("click", () => this.switchTab("adventure") );
+
+        this.buttonTabInventory = document.querySelector("#inventory-button");
+        this.buttonTabInventory.addEventListener("click", () => this.switchTab("inventory") );
+        
+        this.buttonTabOptions = document.querySelector("#options-button");
+        this.buttonTabOptions.addEventListener("click", () => this.switchTab("options") );
+
+        this.tabTown = document.querySelector("#town-tab");
+        this.tabAdventure = document.querySelector("#adventure-tab");
+        this.tabInventory = document.querySelector("#inventory-tab");
+        this.tabOptions = document.querySelector("#options-tab");
+    }
+    
+    switchTab(tabName: string) {
         console.log(this);
-        console.log(something);
+        console.log(tabName);
+
+        this.tabTown.classList.add("hide");
+        this.tabAdventure.classList.add("hide");
+        this.tabInventory.classList.add("hide");
+        this.tabOptions.classList.add("hide");
+
+        switch (tabName) {
+            case "town":
+            default:
+                this.tabTown.classList.remove("hide");
+                break;
+
+            case "adventure":
+                this.tabAdventure.classList.remove("hide");
+                break;
+
+            case "inventory":
+                this.tabInventory.classList.remove("hide");
+                break;
+
+            case "options":
+                this.tabOptions.classList.remove("hide");
+                break;
+        }
     }
 }
